189ff4d27ef5fdb2600ef87fd655629d698a82cf
Neha Anand <nanand@affinionds.com>
2020-05-11 16:40:57 +0530

    folder restructuring

--- Exercise1.js
+++
@@ -1,8 +0,0 @@
-var names = new Array();
-names.push("James Bond");
-names.push("Ethan Hunt");
-names.push("Indiana Jones");
-names.push("Jason Bourn");
-names.sort(function (a, b) { return a.length - b.length; }).reverse().forEach(function (v, i) {
-    console.log(v);
-});
